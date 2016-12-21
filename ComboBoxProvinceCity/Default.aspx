377121c26968f3ff494a5ee19b2740df0b5a8eaa
Nukepayload2 <1939357182@qq.com>
2016-12-22 01:38:47 +0800

    第八题

---
+++ ComboBoxProvinceCity/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ComboBoxProvinceCity.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="LstProvince" AutoPostBack="true" runat="server" OnSelectedIndexChanged="LstProvince_SelectedIndexChanged"></asp:DropDownList>
+            <asp:DropDownList ID="LstCity" runat="server"></asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ComboBoxProvinceCity/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ComboBoxProvinceCity
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        Dictionary<string, string[]> cities = new Dictionary<string, string[]>()
+        {
+            { "陕西",new string[] {"西安", "铜川", "商洛" } },
+            { "河南",new string[] {"许昌", "郑州" } }
+        };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LstProvince.Items.AddRange((from k in cities.Keys select new ListItem(k)).ToArray());
+                LstCity.Items.AddRange((from v in cities["陕西"] select new ListItem(v)).ToArray());
+            }
+        }
+
+        protected void LstProvince_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LstCity.Items.Clear();
+            if (!string.IsNullOrEmpty(LstProvince.SelectedValue))
+            {
+                LstCity.Items.AddRange((from v in cities[LstProvince.SelectedValue] select new ListItem(v)).ToArray());
+            }
+        }
+    }
+}
