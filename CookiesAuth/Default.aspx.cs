0d258459eebdb1b5a5591c4d6e90385eb4f461b7
Nukepayload2 <1939357182@qq.com>
2016-12-22 00:22:18 +0800

    第四题

---
+++ CookiesAuth/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CookiesAuth
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string user = Request.Cookies["User"]?.Value;
+            if (string.IsNullOrEmpty(user))
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                Response.Write($"欢迎，{user}！");
+            }
+        }
+
+        protected void BtnLogout_Click(object sender, EventArgs e)
+        {
+            Response.Cookies["User"].Expires = DateTime.MinValue;
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
