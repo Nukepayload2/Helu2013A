7a415c6613bb7191676a41309438d8f47d754c59
Nukepayload2 <1939357182@qq.com>
2016-12-22 11:17:44 +0800

    第九题

---
+++ ServerTransfer/Query.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerTransfer
+{
+    public partial class Query : System.Web.UI.Page
+    {
+        public int Age { get; private set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Age = 123;
+            Server.Transfer("Result.aspx");
+        }
+    }
+}
---
+++ ServerTransfer/Result.aspx.designer.cs
@@ -0,0 +1,36 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能导致不正确的行为，如果
+//     重新生成代码，则所做更改将丢失。
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace ServerTransfer {
+    
+    
+    public partial class Result {
+        
+        /// <summary>
+        /// form1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// PreviousPage 属性。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的属性。
+        /// </remarks>
+        public new ServerTransfer.Query PreviousPage {
+            get {
+                return ((ServerTransfer.Query)(base.PreviousPage));
+            }
+        }
+    }
+}
